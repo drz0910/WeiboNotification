9805d91cce4b0f38e0e8231148e28f8ef5389335
radoy <dairuzhan@haiziguo.com>
2016-06-01 12:45:22 +0800

    添加项目文件。

---
+++ WeiboNotification/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WeiboNotification
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listViewMessage = new System.Windows.Forms.ListView();
+            this.columnTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnText = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.hideToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip1";
+            this.contextMenuStrip.Size = new System.Drawing.Size(104, 70);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.hideToolStripMenuItem.Text = "Hide";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(604, 305);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // listViewMessage
+            // 
+            this.listViewMessage.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnTime,
+            this.columnText});
+            this.listViewMessage.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewMessage.LabelWrap = false;
+            this.listViewMessage.Location = new System.Drawing.Point(12, 12);
+            this.listViewMessage.Name = "listViewMessage";
+            this.listViewMessage.Size = new System.Drawing.Size(564, 279);
+            this.listViewMessage.TabIndex = 3;
+            this.listViewMessage.UseCompatibleStateImageBehavior = false;
+            this.listViewMessage.View = System.Windows.Forms.View.Details;
+            this.listViewMessage.SelectedIndexChanged += new System.EventHandler(this.listViewMessage_SelectedIndexChanged);
+            // 
+            // columnTime
+            // 
+            this.columnTime.Text = "发表时间";
+            this.columnTime.Width = 120;
+            // 
+            // columnText
+            // 
+            this.columnText.Text = "内容";
+            this.columnText.Width = 400;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 305);
+            this.Controls.Add(this.listViewMessage);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form";
+            this.ShowInTaskbar = false;
+            this.Text = "Notification";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListView listViewMessage;
+        private System.Windows.Forms.ColumnHeader columnTime;
+        private System.Windows.Forms.ColumnHeader columnText;
+    }
+}
+
---
+++ WeiboNotification/Form1.cs
@@ -0,0 +1,235 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeiboNotification
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        DateTime baseDate;
+        private HashSet<long> idSet;
+        private Queue<DateTime> dateQueue;
+        private Queue<string> blogQueue;
+        private Thread piThread;
+        private string siteURL = @"http://m.weibo.cn";
+        private string tagPattern = @"\<a.*?\<\/a\>\s*";
+
+        public Form()
+        {
+            InitializeComponent();
+            baseDate = new DateTime(1970, 1, 1, 8, 0, 0, 0);
+            idSet = new HashSet<long>();
+            dateQueue = new Queue<DateTime>(21);
+            blogQueue = new Queue<string>(21);
+        }
+
+        private void SaveNotification(DateTime date, string blog)
+        {
+            dateQueue.Enqueue(date);
+            blogQueue.Enqueue(blog);
+            if (blogQueue.Count == 20)
+            {
+                dateQueue.Dequeue();
+                blogQueue.Dequeue();
+            }
+        }
+
+        private String expandArticle(String text)
+        {
+            Match match = Regex.Match(text, "href=\\\".*?\\\"");
+            String expandURL = siteURL + text.Substring(match.Index, match.Length).Remove(0, 5).Replace("\"", "");
+            while (true)
+            {
+                System.Net.HttpWebRequest request;
+                // Create HTTP request
+                request = (System.Net.HttpWebRequest)WebRequest.Create(expandURL);
+                System.Net.HttpWebResponse response;
+                try
+                {
+                    response = (System.Net.HttpWebResponse)request.GetResponse();
+
+                }
+                catch (Exception)
+                {
+                    SaveNotification(DateTime.Now, "连接错误");
+                    Thread.Sleep(30000);
+                    continue;
+                }
+                System.IO.StreamReader myreader = new System.IO.StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                string responseText = myreader.ReadToEnd();
+                // scan from right
+                match = Regex.Match(responseText, @"class=.*?weibo-text.*?\<\/div\>", RegexOptions.RightToLeft);
+                responseText = responseText.Substring(match.Index);
+                // scan from left
+                match = Regex.Match(responseText, @"\<\/div\>");
+                responseText = responseText.Substring(0, match.Index);
+                // remove tags
+                if (Regex.IsMatch(responseText, tagPattern))
+                    responseText = Regex.Replace(responseText, tagPattern, "");
+                // remove header
+                return Regex.Replace(responseText, @"class.*\>", "").Trim();
+            }
+        }
+
+        private void RefreshNotification()
+        {
+            while (true)
+            {
+                
+                string mainURL = siteURL + @"/page/card?itemid=1005051821993884_-_WEIBO_INDEX_PROFILE_WEIBO_GROUP_OBJ&callback=_1456298670085_5";
+                String expandPattern = @".?\<a.*?...全文.*?\<\/a\>\s*";
+                System.Net.HttpWebRequest request;
+                // Create HTTP request
+                request = (System.Net.HttpWebRequest)WebRequest.Create(mainURL);
+                System.Net.HttpWebResponse response;
+                try
+                {
+                    response = (System.Net.HttpWebResponse)request.GetResponse();
+                   
+                }
+                catch(Exception)
+                {
+                    SaveNotification(DateTime.Now, "连接错误");
+                    Thread.Sleep(30000);
+                    continue;
+                }
+                System.IO.StreamReader myreader = new System.IO.StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                string responseText = myreader.ReadToEnd().Replace("_1456298670085_5(", "").Replace(")", "");
+                myreader.Close();
+                JObject obj = JObject.Parse(responseText);
+                if(obj.Count < 5)
+                {
+                    SaveNotification(DateTime.Now, "解析错误");
+                    Thread.Sleep(30000);
+                    continue;
+                }
+                IList<JToken> list = obj["card_group"].Children().ToList();
+                long id;
+                DateTime date;
+                string str;
+                for (int i = list.Count - 1; i >= 0; i--)
+                {
+                    JToken blog = list[i]["mblog"];
+                    if (blog != null && blog["isTop"] == null)
+                    {
+                        id = long.Parse(blog["id"].ToString());
+                        if (!idSet.Contains(id))
+                        {
+                            idSet.Add(id);
+                            date = baseDate.AddSeconds(long.Parse(blog["created_timestamp"].ToString()));
+                            str = blog["text"].ToString();
+                            // check if need to expand
+                            if (Regex.IsMatch(str, expandPattern))
+                                str = expandArticle(str.Substring(Regex.Match(str, expandPattern, RegexOptions.RightToLeft).Index));
+                            // remove tags
+                            else if (Regex.IsMatch(str, tagPattern))
+                                str = Regex.Replace(str, tagPattern, "");
+
+                            SaveNotification(date, str);
+                            notifyIcon.ShowBalloonTip(20000, date.ToShortTimeString(), str, ToolTipIcon.Info);
+                        }
+                    }
+                }
+                if (InvokeRequired)
+                {
+                    Invoke(new InvokeDelegate(RefreshView));
+                }
+
+                DateTime now = DateTime.Now;
+                if(now.Hour < 8 && now.Hour > 15)
+                {
+                    Thread.Sleep(3000000);
+                }else
+                {
+                    Thread.Sleep(60000);
+                }
+                
+            }
+        }
+        private delegate void InvokeDelegate();
+        private void RefreshView()
+        {
+            Queue<DateTime>.Enumerator dateEnum = dateQueue.GetEnumerator();
+            Queue<string>.Enumerator blogEnum = blogQueue.GetEnumerator();
+
+            listViewMessage.Items.Clear();
+            while (dateEnum.MoveNext() && blogEnum.MoveNext())
+            {
+                String dateStr = dateEnum.Current.ToShortTimeString() + ' ' + dateEnum.Current.ToShortDateString();
+                String blogStr = blogEnum.Current;
+                int wrap = 35;
+                int index = wrap;
+                if(blogStr.Length < index)
+                {
+                    listViewMessage.Items.Add(new ListViewItem(new string[] { dateStr, blogStr }));
+                }
+                else
+                {
+                    listViewMessage.Items.Add(new ListViewItem(new string[] { dateStr, blogStr.Substring(0, wrap) }));
+                    while (blogStr.Length > index + wrap)
+                    {
+                        listViewMessage.Items.Add(new ListViewItem(new string[] { "", blogStr.Substring(index, wrap) }));
+                        index += wrap;
+                    }
+                    
+                    listViewMessage.Items.Add(new ListViewItem(new string[] { "", blogStr.Substring(index) }));
+                }
+               
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            piThread = new Thread(new ThreadStart(RefreshNotification));
+            piThread.Start();
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.Visible)
+            {
+                hideToolStripMenuItem_Click(sender, e);
+            }
+            else
+            {
+                openToolStripMenuItem_Click(sender, e);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void listViewMessage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://weibo.com/u/1821993884?is_all=1");
+        }
+
+        private void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            piThread.Abort();
+        }
+    }
+}
